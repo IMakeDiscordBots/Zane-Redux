60870b27741eb254b44fe4c8bf1f9288d88a6782
IMakeDiscordBots <77121499+IMakeDiscordBots@users.noreply.github.com>
2022-09-06 12:49:02 -0500

    Created delete command functions.

--- deploy-command.js
+++ deploy-command.js
@@ -14,3 +14,26 @@ const rest = new REST({ version: '10' }).setToken(token);
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
     .then((data) => console.log(`Successfully registered ${data.length} application command(s).`))
     .catch(console.error);
+
+//Guild level
+rest.delete(Routes.applicationGuildCommands(clientId, guildId, 'commandId'))
+    .then(() => console.log('Sucessfully deleted guild command.'))
+    .catch(console.error);
+
+//Global level
+rest.delete(Routes.applicationCommand(clientId, 'commandId'))
+    .then(() => console.log('Sucessfully deleted guild command.'))
+    .catch(console.error);
+
+//Delete all but for guild level
+rest.put(Routes.applicationGuildCommands(clientId, guildId, { body: [] }))
+    .then(() => console.log('Sucessfully deleted all guild commands.'))
+    .catch(console.error);
+
+//Delete all but for global level
+rest.put(Routes.applicationCommand(clientId, 'commandId', { body: [] }))
+    .then(() => console.log('Sucessfully deleted all application  commands.'))
+    .catch(console.error);
+
+//Delete all
+
