5eb972ac6920ee44116ee9a6a891758b20161111
IMakeDiscordBots <77121499+IMakeDiscordBots@users.noreply.github.com>
2022-10-14 15:07:41 -0500

    Fixed misspelled command on line 6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
@@ -12,7 +12,6 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-
     client.commands.set(command.data.name, command);
 }
 
@@ -24,7 +23,7 @@ for (const file of eventFiles) {
     const event = require(filePath);
     
     if (event.once) {
-        const filePath = path.join(event.name, (...args) => event.execute(...args));
+        client.once(event.name, (...args) => event.execute(...args));
     } else {
         client.on(event.name, (...args) => event.execute(...args));
     }
