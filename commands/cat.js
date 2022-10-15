303c6e5a98b1d1bce8b10edd7061b097d4cd7b19
IMakeDiscordBots <aryanmohanty1335@gmail.com>
2022-10-15 13:58:35 -0500

    Updated discordjs version and created cat command

---
+++ commands/cat.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('cat')
+        .setDescription('Replies with a random image of a cat!'),
+    async execute(interaction) {
+        const catImgEmbed = new EmbedBuilder()
+            .setImage('https://cataas.com/c')
+            .setFooter({ text: 'Images courtesy of https://cataas.com/', iconURL: 'https://cdn-icons-png.flaticon.com/512/616/616430.png' })
+        return interaction.reply({ embeds: [catImgEmbed] });
+    },
+};
