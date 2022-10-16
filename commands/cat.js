ea1a1be3434dbc32c75317682ec1169dac6fe3d8
IMakeDiscordBots <aryanmohanty1335@gmail.com>
2022-10-15 19:03:03 -0500

    Fixed cat.js file

--- commands/cat.js
+++ commands/cat.js
@@ -6,7 +6,7 @@ module.exports = {
         .setDescription('Replies with a random image of a cat!'),
     async execute(interaction) {
         const catImgEmbed = new EmbedBuilder()
-            .setImage('https://cataas.com/c')
+            .setImage(`https://cataas.com/c?${Date.now()}`)
             .setFooter({ text: 'Images courtesy of https://cataas.com/', iconURL: 'https://cdn-icons-png.flaticon.com/512/616/616430.png' })
         return interaction.reply({ embeds: [catImgEmbed] });
     },
