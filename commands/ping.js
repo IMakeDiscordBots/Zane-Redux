d3a9d29ac86b057982980a68ecd7e5626fffdadb
IMakeDiscordBots <aryanmohanty1335@gmail.com>
2022-10-17 15:22:15 -0500

    Started ping embed

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,14 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with current ping.'),
     async execute(interaction) {
-        //await interaction.reply(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
         const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+        const pingEmbed = new EmbedBuilder()
+            .setColor(`#${Math.floor(Math.random()*16777215).toString(16)}`)
+            .setFooter(`:ping_pong`);
         await interaction.editReply(`:ping_pong: Pong!\n:stopwatch: Uptime: ${Math.round(interaction.client.uptime / 60000)} minutes\n:sparkling_heart: Websocket heartbeat: ${interaction.client.ws.ping}ms.\n:round_pushpin: Roundtrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`)
     },
 }
