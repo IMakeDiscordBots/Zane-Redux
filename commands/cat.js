5802fcd2a15df26c2cb8edfa6f968266c82fc54d
IMakeDiscordBots <aryanmohanty1335@gmail.com>
2022-11-07 14:37:02 -0600

    Remaking cat command using aws

--- commands/cat.js
+++ commands/cat.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { request } = require('undici');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,9 +7,11 @@ module.exports = {
         .setDescription('Replies with a random image of a cat!'),
     async execute(interaction) {
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        const catResult = await request('https://aws.random.cat/meow');
+        const { file } = await catResult.body.json();
         const catImgEmbed = new EmbedBuilder()
             .setColor(`#${randomColor}`)
-            .setImage(`https://cataas.com/c?${Date.now()}`)
+            .setImage({ files: [{ attachment: file, name: 'cat.png' }] })
             .setFooter({ text: 'Images courtesy of https://cataas.com/', iconURL: 'https://cdn-icons-png.flaticon.com/512/616/616430.png' })
         return interaction.reply({ embeds: [catImgEmbed] });
     },
