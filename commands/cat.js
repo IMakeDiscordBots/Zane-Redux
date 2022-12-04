67aee1a8b8c0e8e0b2da094be948678672cbe171
IMakeDiscordBots <77121499+IMakeDiscordBots@users.noreply.github.com>
2022-12-04 00:25:46 +0000

    Temporarily "reverted" cat command

--- commands/cat.js
+++ commands/cat.js
@@ -4,14 +4,27 @@ const { request } = require('undici');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('cat')
-        .setDescription('Replies with a random image of a cat!'),
+        .setDescription('Replies with a random image of a cat!')
+        .addStringOption(option => option.setName('speak').setDescription('What you\'d like the image to say!')),
     async execute(interaction) {
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        const input = interaction.options.getString('speak')
+        const catImgEmbed = 
+            (input) ? 
+                new EmbedBuilder()
+                    .setColor(`#${randomColor}`)
+                    .setImage(`https://cataas.com/c/s/${input}?${Date.now()}`)
+                    .setFooter({ text: 'Images courtesy of https://cataas.com/', iconURL: 'https://cdn-icons-png.flaticon.com/512/616/616430.png' }) 
+                : new EmbedBuilder()
+                    .setColor(`#${randomColor}`)
+                    .setImage(`https://cataas.com/c?${Date.now()}`)
+                    .setFooter({ text: 'Images courtesy of https://cataas.com/', iconURL: 'https://cdn-icons-png.flaticon.com/512/616/616430.png' })
+        return interaction.reply({ embeds: [catImgEmbed] });
+        /* WHY WONT YOU WORK
         const catResult = await request('https://aws.random.cat/meow');
         const { file } = await catResult.body.json();
         const attachment = new AttachmentBuilder(file, 'cat.png');
-        
-        /*
+
         const catImgEmbed = new EmbedBuilder()
             .setColor(`#${randomColor}`)
             .setImage(`https://cataas.com/c?${Date.now()}`)
@@ -19,10 +32,5 @@ module.exports = {
         */
 
         //Can't test because aws.random.cat/meow keeps returning 503 error :agony:
-        const catImgEmbed = new EmbedBuilder()
-            .setColor(`#${randomColor}`)
-            .setImage('')
-            .setFooter({ text: 'Images courtesy of https://cataas.com/', iconURL: 'https://cdn-icons-png.flaticon.com/512/616/616430.png' })
-        return interaction.reply({ embeds: [catImgEmbed] });
     },
 };
